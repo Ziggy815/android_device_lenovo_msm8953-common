1b111985a72386c416ac55e2119868eb033704ba
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-01-19 12:40:20 +0100

    msm8953: Disable compressed APEX
    Compressed APEX has been introduced in Android 12, and they are enabled
    by default inheriting updatable_apex.mk.
    Unfortunately, when the device is encrypted, they are causing bootloops
    on msm8953.
    As the only advantage is some space gain on disk, we can
    simply disable them: keep the variable immediatly after including the
    updatable_apex.mk to avoid being overwritten.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: I410bd4ad8d589971339dddab709c5b40d9c9a15c

--- msm8953.mk
+++ msm8953.mk
@@ -29,6 +29,9 @@ TARGET_BOARD_SUFFIX := _64
 # Device is a Tablet
 PRODUCT_CHARACTERISTICS := tablet
 
+# Apex
+PRODUCT_COMPRESSED_APEX := false
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
